55b9bec6eb211adc8ce7b936468e483529ea1304
Vyacheslav Chigrin <vyacheslav.chigrin@yandex.ru>
2021-11-28 18:21:12 +0300

    Better failed assertions handling.

--- app/verify.cc
+++ app/verify.cc
@@ -2,23 +2,22 @@
 // Use of this source code is governed by a GPLv3 license that can be
 // found in the LICENSE file.
 
-#pragma once
-#include <string_view>
+#include "app/verify.h"
+
+#include <cstdlib>
+#include <iostream>
 
 namespace m_time_tracker {
-// Few macros to always abort program in case any error condition encountered.
 
-[[noreturn]]
 void HandleAssertionFailure(
     const std::string_view expr,
     const std::string_view file,
-    int line) noexcept;
-
-#define VERIFY(expr) \
-  if (!(expr)) { \
-    ::m_time_tracker::HandleAssertionFailure(#expr, __FILE__, __LINE__); \
-  }
-
-#define NOTREACHED() VERIFY(false)
+    int line) noexcept {
+  std::cerr << "Assertion " << expr
+            << " failed at "
+            << file << ":" << line
+            << std::endl;
+  std::abort();
+}
 
 }  // namespace m_time_tracker
